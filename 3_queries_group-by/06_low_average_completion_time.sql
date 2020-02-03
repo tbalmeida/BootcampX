7b97b72ec0f24321d028689291dfbc5124d019d3
tbalmeida <tbalmeida@gmail.com>
2020-02-03 23:31:44 +0000

    Last query for D1

--- 3_queries_group-by/06_low_average_completion_time.sql
+++ 3_queries_group-by/06_low_average_completion_time.sql
@@ -4,5 +4,5 @@ INNER JOIN assignment_submissions ss ON s.id = ss.student_id
 INNER JOIN assignments a ON a.id = ss.assignment_id
 WHERE s.end_date IS NULL
 GROUP BY s.name
-HAVING avg(ss.duration) < a.duration
+HAVING avg(ss.duration) < avg(a.duration)
 ORDER BY avg(ss.duration) ASC;
